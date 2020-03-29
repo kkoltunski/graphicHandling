b0f139ab971b97027160dce67b773c9ed7ceed89
Klaudiusz Kołtuński <kkoltunski@sii.pl>
2020-03-29 20:23:15 +0200

    Rebuilded functions for .bmp files 8/16/32 as well

--- BMPpicture.cpp
+++ BMPpicture.cpp
@@ -31,12 +31,12 @@ bool BMPpicture::checkFile(){
 		if(validateSignature(2)){
 			getSettings();
 			
-			if(picSettings.biCompression != 0){		//Decompression is necessary
+			if(picSettings.biCompression){			//Decompression is necessary
 				std::cout << "Decompression necessary" << std::endl;
 			}
 			
-			if(picSettings.biBitCount != 24){		//Color table is necessary
-				std::cout << "Color table necessary" << std::endl;
+			if(picSettings.biClrUsed){				//Color table is necessary
+				getColorIndex();
 			}
 			
 			std::cout << "BMP PICTURE SETTINGS";
@@ -77,57 +77,102 @@ void BMPpicture::getSettings(){
 	pic.read((char*)&picSettings.biClrImportant, 4);
 }
 
-void BMPpicture::makeHeader(){
-//FILE HEADER INFO
-	operationResult.write((char*)&picIntro.bfType, 2);
-	operationResult.write((char*)&picIntro.bfSize, 4);
-	operationResult.write((char*)&picIntro.bfReserved1, 2);
-	operationResult.write((char*)&picIntro.bfReserved2, 2);
-	operationResult.write((char*)&picIntro.bfOffBits, 4);
-
-//IMAGE HEADER INFO
-	operationResult.write((char*)&picSettings.biSize, 4);
-	operationResult.write((char*)&picSettings.biWidth, 4);
-	operationResult.write((char*)&picSettings.biHeight, 4);
-	operationResult.write((char*)&picSettings.biPlanes, 2);
-	operationResult.write((char*)&picSettings.biBitCount, 2);
-	operationResult.write((char*)&picSettings.biCompression, 4);
-	operationResult.write((char*)&picSettings.biSizeImage, 4);
-	operationResult.write((char*)&picSettings.biXPelsPerMeter, 4);
-	operationResult.write((char*)&picSettings.biYPelsPerMeter, 4);
-	operationResult.write((char*)&picSettings.biClrUsed, 4);
-	operationResult.write((char*)&picSettings.biClrImportant, 4);
+void BMPpicture::getColorIndex(){
+	/*color table is array which contain indexed colors of pixel.
+	Each pixel has his own index which point here.*/
+	short pixelFormat = ((picIntro.bfOffBits - 54) / picSettings.biClrUsed);	//Result should be 4(BRGA) or 3(BRG) depend on pixel representation //54 is pos after headers
+	pic.seekg(54, std::ios_base::beg);											//color table begining
+	for(;pic.tellg() != picIntro.bfOffBits;){									//save positions of each index in vector 
+		colorTable.push_back(pic.tellg());
+		pic.seekg(pixelFormat, std::ios_base::cur);
+	}
 }
 
-std::ofstream& BMPpicture::negative(){
-	string resultFilePath = filePath;					//Prepare file path for output file
-	resultFilePath = resultFilePath.insert(resultFilePath.find(".bmp"), "_negative");
+bool BMPpicture::makeFile(string _operation){
+	bool result;
+	string resultFilePath = filePath;									//Prepare file path for output file
+	resultFilePath = resultFilePath.insert(resultFilePath.find(".bmp"), "_" + _operation);
 	
 	operationResult.close();
 	operationResult.open(resultFilePath, std::ios_base::out | std::ios_base::trunc | std::ios_base::binary);
 	
 	if(operationResult.is_open()){
-		if(!operationResult.fail())	makeHeader();
-		else std::cout << "Fail appear." << std::endl;
-	}
-	else std::cout << "File is not opened." << std::endl;	
-	
-	pic.seekg(picIntro.bfOffBits, std::ios_base::beg);	
-	
-	for(int y = 0; y <= picSettings.biHeight; ++y){
-		for(int x = 0; x <= picSettings.biWidth; ++x){
-			auto pToPix = new pixel;
-			pic.read((char*)pToPix, 3);
-					
-			pToPix->Blue ^= 0xFF;
-			pToPix->Green ^= 0xFF;
-			pToPix->Red ^= 0xFF;
+		if(!operationResult.fail()){
+			//FILE HEADER INFO
+			operationResult.seekp(0, std::ios_base::beg);					//File header begining
+			operationResult.write((char*)&picIntro.bfType, 2);
+			operationResult.write((char*)&picIntro.bfSize, 4);
+			operationResult.write((char*)&picIntro.bfReserved1, 2);
+			operationResult.write((char*)&picIntro.bfReserved2, 2);
+			operationResult.write((char*)&picIntro.bfOffBits, 4);
+
+			//IMAGE HEADER INFO
+			operationResult.write((char*)&picSettings.biSize, 4);
+			operationResult.write((char*)&picSettings.biWidth, 4);
+			operationResult.write((char*)&picSettings.biHeight, 4);
+			operationResult.write((char*)&picSettings.biPlanes, 2);
+			operationResult.write((char*)&picSettings.biBitCount, 2);
+			operationResult.write((char*)&picSettings.biCompression, 4);
+			operationResult.write((char*)&picSettings.biSizeImage, 4);
+			operationResult.write((char*)&picSettings.biXPelsPerMeter, 4);
+			operationResult.write((char*)&picSettings.biYPelsPerMeter, 4);
+			operationResult.write((char*)&picSettings.biClrUsed, 4);
+			operationResult.write((char*)&picSettings.biClrImportant, 4);
 			
-			operationResult.write((char*)pToPix, 3);
-			delete pToPix;
+			result = 1;
+		}
+		else{
+			std::cout << "Fail appear." << std::endl; 
+			result = 0;
 		}
 	}
-		
+	else{
+		std::cout << "File is not opened." << std::endl; 
+		result = 0;
+	}
+	
+	return result;
+}
+
+std::ofstream& BMPpicture::negative(){
+	if(!makeFile("negative")){
+		std::cout << "Negative operation unsucessfull.\n" << std::endl;
+		return operationResult;
+	}
+	
+	if(picSettings.biClrUsed) pic.seekg(54, std::ios_base::beg);			//for color table inversion
+	else pic.seekg(picIntro.bfOffBits, std::ios_base::beg);					//for pixels direct inversion
+	
+	auto pToPix = new pixelBGR;												//auxiliary struct
+				
+	for(int y = 0; y <= picSettings.biHeight; ++y){							//Pixels net (height)
+		for(int x = 0; x <= picSettings.biWidth; ++x){						//Pixels net (width)
+			if(picSettings.biClrUsed && pic.tellg() == 54){					//color table inversion (if exist - only in first entrance to loop when there is color table and reading pointer is set at 54pos)
+				for(;pic.tellg() < picIntro.bfOffBits;){					
+					pic.read((char*)pToPix, 4);	
+					pToPix->Blue ^= 0xFF;
+					pToPix->Green ^= 0xFF;
+					pToPix->Red ^= 0xFF;
+					operationResult.write((char*)pToPix, 4);
+				}
+			}
+			
+			if(picSettings.biBitCount != 24){								//For all cases except 24bpp we ned to just copy-paste pixels (color table is already inverted)
+				char tempB;
+				pic.read((char*)&tempB, 1);
+				operationResult.write((char*)&tempB, 1);
+			}
+			else{															//For 24bpp direct change at orginal pixel is necessary
+				pic.read((char*)pToPix, 3);			
+				pToPix->Blue ^= 0xFF;
+				pToPix->Green ^= 0xFF;
+				pToPix->Red ^= 0xFF;
+				operationResult.write((char*)pToPix, 3);
+			}	
+		}
+	}
+	
+	delete pToPix;	
 	std::cout << "Negative operation done.\n" << std::endl;
 	return operationResult;
 }
